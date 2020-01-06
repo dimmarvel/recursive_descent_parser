adf92e24bed21dbe300192f4757a7032eab34c3c
Matsiukhov Dzmitry <dimmarvel@mail.ru>
2020-01-06 14:53:31 +0300

    Parser.h complete.

--- RecursiveParser/Parser.h
+++ RecursiveParser/Parser.h
@@ -1,1 +1,14 @@
+#ifndef _PARSER_H_
+#define _PARSER_H_
+#include "lex_analyzer.h"
 
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+void parser_input(char*input);
+static int stmt(lex_analyzer token);
+static int expr(lex_analyzer token);
+static int term(lex_analyzer token);
+static int fact(lex_analyzer token);
+#endif
--- RecursiveParser/mainq.cpp
+++ RecursiveParser/mainq.cpp
@@ -1,7 +1,7 @@
 #include "Parser.h"
 int main(int argc,char** argv) {
 	setlocale(LC_ALL, "RUS");
-	parser(argv[1] + '\0');
+	parser_input(argv[1] + '\0');
 	system("pause");
 	return 0;
 }
